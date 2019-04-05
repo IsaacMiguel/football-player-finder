31dbd179141b3ea6c2fb4e4ca10ccd14c0ab8a01
isaacmiguel <isaacmiguel@gmail.com>
2019-04-05 12:09:42 -0300

    Desacoplando logica del formulario en el container, control del submit, envio de valores por defecto/iniciales y valores para el field del tipo select

--- src/containers/FootballPlayerFinder.js
+++ src/containers/FootballPlayerFinder.js
@@ -4,14 +4,52 @@ import Header from './../components/Header';
 import FinderPlayerForm from './../components/FinderPlayerForm';
 import TableResultFinder from './../components/TableResultFinder';
 
+const initialValues = {
+  playerName: '', // ex: Leonel Messi
+  position: '', // ex: Central Midfield
+  age: '' // ex: 18
+};
+
+const selectOptions = [
+  { value:"", label: "Position" },
+  { value:"Attacking Midfield", label: "Attacking Midfield" },
+  { value:"Central Midfield", label: "Central Midfield" },
+  { value:"Centre-Back", label: "Centre-Back" },
+  { value:"Centre-Forward", label: "Centre-Forward" },
+  { value:"Defensive Midfield", label: "Defensive Midfield" },
+  { value:"Keeper", label: "Keeper" },
+  { value:"Left Midfield", label: "Left Midfield" },
+  { value:"Left Wing", label: "Left Wing" },
+  { value:"Left-Back", label: "Left-Back" },
+  { value:"Right-Back", label: "Right-Back" }
+];
+
 class FootballPlayerFinder extends Component {
+  constructor() {
+    super();
+
+    document.title = 'Site Title';
+  }
+
+  handleOnSubmit = (values, actions) => {
+    setTimeout(function() { 
+      console.log(values)
+      console.log('llamada a la api')
+      actions.setSubmitting(false); 
+    }, 2000);
+  }
+
   render() {
     return(
       <div>
         <h1>FootballPlayerFinder Container encargado de la logica</h1>
         <h1>A continuacion abajo, los componentes encargados solamente del renderizado</h1>
         <Header />
-        <FinderPlayerForm />
+        <FinderPlayerForm 
+          onSubmit={this.handleOnSubmit}
+          initialValues={initialValues}
+          selectOptions={selectOptions}
+        />
         <TableResultFinder />
       </div>
     )
