1701e1729e105544459a58b77da0cd5944812096
isaacmiguel <isaacmiguel@gmail.com>
2019-04-05 10:06:43 -0300

    Add directorio del store

---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+import { createStore, compose } from 'redux';
+import reducers from './../reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
+
+export const store = createStore(() => reducers, {}, composeEnhancers());
