6577f135a1db55a5b3a001e32af30f26c11905c1
isaacmiguel <isaacmiguel@gmail.com>
2019-04-05 12:08:11 -0300

    Add formulario con formik, desacoplando en la funcion render el select de opciones

--- src/components/FinderPlayerForm.js
+++ src/components/FinderPlayerForm.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Formik, Field, Form } from 'formik';
 
-const FinderPlayerForm = props => {
+const renderOptions = values => values.map(({ value, label }) => <option key={value} value={value} label={label} />)
+
+const FinderPlayerForm = ({ onSubmit, initialValues, selectOptions }) => {
   return(
-    <h2>Formulario de busqueda</h2>
+    <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        render={({ errors, status, touched, isSubmitting }) => (
+          <Form>
+            <Field type="text" name="playerName" placeholder="Player Name"/>
+            <Field component="select" name="position">
+              {
+                renderOptions(selectOptions)
+              }
+            </Field>
+            <Field type="text" name="age" placeholder="Age" />
+            <button type="submit" disabled={isSubmitting}>
+              Search
+            </button>
+          </Form>
+        )}
+      />
   )
 };
 
 FinderPlayerForm.propTypes = {
-
+  onSubmit: PropTypes.func.isRequired,
+  initialValues: PropTypes.object.isRequired,
+  selectOptions: PropTypes.array.isRequired
 };
 
 export default FinderPlayerForm;
