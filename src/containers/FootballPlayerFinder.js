250da4defda4b000aee0a8c89ebad58e43501407
isaacmiguel <isaacmiguel@gmail.com>
2019-04-05 12:41:15 -0300

    Conectamos el container al store, le agregamos el action fetch y desacoplamos un poco mas en constantes

--- src/containers/FootballPlayerFinder.js
+++ src/containers/FootballPlayerFinder.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from './../components/Header';
 import FinderPlayerForm from './../components/FinderPlayerForm';
 import TableResultFinder from './../components/TableResultFinder';
+import { SELECT_OPTIONS } from './../constants';
+import { fetchFootballPlayers } from './../actions/fetchFootballPlayers';
 
 const initialValues = {
   playerName: '', // ex: Leonel Messi
   position: '', // ex: Central Midfield
   age: '' // ex: 18
 };
-
-const selectOptions = [
-  { value:"", label: "Position" },
-  { value:"Attacking Midfield", label: "Attacking Midfield" },
-  { value:"Central Midfield", label: "Central Midfield" },
-  { value:"Centre-Back", label: "Centre-Back" },
-  { value:"Centre-Forward", label: "Centre-Forward" },
-  { value:"Defensive Midfield", label: "Defensive Midfield" },
-  { value:"Keeper", label: "Keeper" },
-  { value:"Left Midfield", label: "Left Midfield" },
-  { value:"Left Wing", label: "Left Wing" },
-  { value:"Left-Back", label: "Left-Back" },
-  { value:"Right-Back", label: "Right-Back" }
-];
 
 class FootballPlayerFinder extends Component {
   constructor() {
@@ -32,23 +21,22 @@ class FootballPlayerFinder extends Component {
   }
 
   handleOnSubmit = (values, actions) => {
-    setTimeout(function() { 
+    setTimeout(() => { 
       console.log(values)
       console.log('llamada a la api')
+      this.props.fetchFootballPlayers();
       actions.setSubmitting(false); 
-    }, 2000);
+    }, 1000);
   }
 
   render() {
     return(
       <div>
-        <h1>FootballPlayerFinder Container encargado de la logica</h1>
-        <h1>A continuacion abajo, los componentes encargados solamente del renderizado</h1>
         <Header />
         <FinderPlayerForm 
           onSubmit={this.handleOnSubmit}
           initialValues={initialValues}
-          selectOptions={selectOptions}
+          selectOptions={SELECT_OPTIONS}
         />
         <TableResultFinder />
       </div>
@@ -57,7 +45,11 @@ class FootballPlayerFinder extends Component {
 };
 
 FootballPlayerFinder.propTypes = {
-
+  fetchFootballPlayers: PropTypes.func.isRequired
 };
 
-export default FootballPlayerFinder;
+const mapDispatchToProps = {
+  fetchFootballPlayers
+};
+
+export default connect(null, mapDispatchToProps)(FootballPlayerFinder);
